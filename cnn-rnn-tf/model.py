4562c83837ca0a56370b77fd30d2491a3b23e2ef
cs-Zeyu <shengzeyu19_98@163.com>
2020-03-06 22:08:34 +0800

    train draft

--- cnn-rnn-tf/model.py
+++ cnn-rnn-tf/model.py
@@ -65,7 +65,6 @@ class CNN_Encoder():
         self.conv4_3 = self.conv("conv4rwe_3",self.conv4_2,512,trainable=False)
         self.pool4 = self.maxpool("pool4",self.conv4_3,trainable=False)
  
- 
         #conv5
         self.conv5_1 = self.conv("conv5_1",self.pool4,512,trainable=False)
         self.conv5_2 = self.conv("convrwe5_2",self.conv5_1,512,trainable=False)
@@ -91,15 +90,21 @@ class Decoder():
         self.feats = feats
         self.captions = captions
         # self.lengths = lengths
-
+        self.batch_size = 32
         self.parameters = []
-        self.vocab_size = None  # count later
+        self.vocab_size = 24  # count later
         self.vis_dim = 512
         self.vis_num = 196
         self.hidden_dim = 1024
 
-        self.a_feat = None
-        self.a_hiddens = None
+        self.attention()
+        self.decolayer()
+
+    def saver(self):
+        # variables_to_restore = tf.contrib.framework.get_variables_to_restore(include=['rcnn'])
+        # saver = tf.train.Saver(variables_to_restore)
+        # return saver
+        return tf.train.Saver(self.parameters)
         
     def conv(self,name, input_data, out_channel, trainable, bias=True):
         in_channel = input_data.get_shape()[-1]
@@ -115,41 +120,54 @@ class Decoder():
 
 
     def attention(self):
+        # self att biaas move to init
         self.att_bias = tf.get_variable("att_bias", [self.vis_num], dtype=tf.float32,trainable=True) #should be zero
+        self.parameters += [self.att_bias]
         self.att_vw = self.conv("att_vw",self.feats,self.vis_dim,trainable=True,bias=False)
-        self.att_hw = self.conv("att_hw",self.a_hiddens,self.vis_dim,trainable=True,bias=False) #unsqeeze
+        self.att_hw = self.conv("att_hw",self.hx,self.vis_dim,trainable=True,bias=False) #unsqeeze
         # self.att_bias = 
-        self.att_relu = tf.nn.relu(self.att_vw + self.att_hw + self.att_bias, name="att_full")
+        self.att_relu = tf.nn.relu(self.att_vw + tf.expand_dims(self.att_hw, 1) + tf.reshape(self.att_bias, [1, -1, 1]), name="att_full")
         self.att_w = self.conv("att_w", self.att_relu, 1, trainable=True,bias=False)
         self.att_alpha = tf.nn.softmax(self.att_w) # dim=1
-        self.context = tf.reduce_sum(tf.matmul(self.a_feat, self.att_alpha), 1)
+        self.context = tf.reduce_sum(tf.matmul(self.feats, self.att_alpha), 1)
         
 
     def decolayer(self):
         self.embeddings = tf.get_variable("embedding", [self.vocab_size, 512])
+        self.parameters += [self.embeddings]
         self.cap_embed = tf.nn.embedding_lookup(self.embeddings, self.captions)
-        self.mean_feats = tf.reduce_mean(self.feats, 1) # unsqueeze
+        self.mean_feats = tf.expand_dims(tf.reduce_mean(self.feats, 1), 1) # unsqueeze
         self.concat_embedding = tf.concat([self.mean_feats, self.cap_embed], 1)
+
+        self.hx = tf.get_variable("hx", [self.batch_size, 1024])
+        self.cx = tf.get_variable("cx", [self.batch_size, 1024])
+        self.linear = self.conv("linear",self.hx,self.vocab_size,trainable=True,bias=True)
 
         cell_fun = rnn_cell.BasicLSTMCell
         cell = cell_fun(1024, state_is_tuple=True)
         self.cell = rnn_cell.MultiRNNCell([cell], state_is_tuple=True)
-        state = self.cell.zero_state(1024, tf.float32) # !!
+        self.parameters += [self.cell.weights]
+        self.init_cell_state = self.cell.zero_state(1, tf.float32) # !!
         
-        output, last_state = tf.nn.dynamic_rnn(cell, self.concat_embedding, initial_state=state, scope="rnn")
-        output = tf.nn.dropout(output, 0.5)
-        self.softmax_w = tf.get_variable("softmax_w", [1024, self.vocab_size])
-        self.softmax_b = tf.get_variable("softmax_b", [self.vocab_size])
-        logits = tf.matmul(output, self.softmax_w) + self.softmax_b
-        probs = tf.nn.softmax(logits)
+        # output, last_state = tf.nn.dynamic_rnn(cell, self.concat_embedding, initial_state=state, scope="rnn")
+        # output = tf.nn.dropout(output, 0.5)
+        # self.softmax_w = tf.get_variable("softmax_w", [1024, self.vocab_size])
+        # self.softmax_b = tf.get_variable("softmax_b", [self.vocab_size])
+        # logits = tf.matmul(output, self.softmax_w) + self.softmax_b
+        # probs = tf.nn.softmax(logits)
 
-    def caption_gen(self):
-        batch_size, time_step = self.captions.size() #   may have problems
-        self.a_hiddens = tf.zeros(batch_size, 1024)
-        for i in range(time_step):
-            feas = self.context
-            input = tf.concat([feas, self.concat_embedding[:,i,:]], -1) #  may have problem
-            # self.a_hiddens,_ = self.lstm(input) # !!
-            self.a_hiddens, state = tf.nn.dynamic_rnn(self.cell, input, time_major=False)
-            logits = tf.matmul(self.a_hiddens, self.softmax_w) + self.softmax_b
-            probs = tf.nn.softmax(logits)
+    def load_weights(self, weight_file, sess):
+        self.saver().restore(sess, weight_file)
+        print("-----------decoder loaded---------------")
+
+
+    # def caption_gen(self):
+    #     batch_size, time_step = self.captions.size() #   may have problems
+    #     self.a_hiddens = tf.zeros(batch_size, 1024)
+    #     for i in range(time_step):
+    #         feas = self.context
+    #         input = tf.concat([feas, self.concat_embedding[:,i,:]], -1) #  may have problem
+    #         # self.a_hiddens,_ = self.lstm(input) # !!
+    #         self.a_hiddens, state = tf.nn.dynamic_rnn(self.cell, input, time_major=False)
+    #         logits = tf.matmul(self.a_hiddens, self.softmax_w) + self.softmax_b
+    #         probs = tf.nn.softmax(logits)
--- cnn-rnn-tf/train.py
+++ cnn-rnn-tf/train.py
@@ -4,7 +4,7 @@ import os
 import argparse
 import pickle
 from model import CNN_Encoder, Decoder
-
+from read_record import get_iter
 
 shuffle_pool_size = 4000
 
@@ -12,14 +12,20 @@ shuffle_pool_size = 4000
 
 
 # pre-process image for training
-def pre_porcess(img):
-    image = tf.gfile.FastGFile('./1.jpg', 'rb').read()
-    decoded_image = tf.image.decode_jpeg(image, channels=3)
-    converted_image = tf.image.convert_image_dtype(decoded_image, tf.float32)
-    image = tf.image.random_flip_left_right(converted_image)
-    image = tf.image.random_flip_up_down(image)
-    std_img = tf.image.per_image_standardization(img)
-    return std_img
+def pre_process(decoded_image):
+    # image = tf.gfile.FastGFile('./1.jpg', 'rb').read()
+    # decoded_image = tf.image.decode_jpeg(image, channels=3)
+    for i in range(32):
+        converted_image = tf.image.convert_image_dtype(decoded_image[i], tf.float32)
+        converted_image = tf.random_crop(converted_image, [224, 224, 3])
+        image = tf.image.random_flip_left_right(converted_image)
+        image = tf.image.random_flip_up_down(image)
+        std_img = tf.image.per_image_standardization(image)
+        if i == 0:
+            imgs = [std_img]
+        else:
+            imgs = tf.concat([imgs, [std_img]], 0)
+    return imgs
 
 
 # eval
@@ -32,33 +38,57 @@ def main(args):
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
     with tf.Session(config=config) as sess:
-        data_set_train = get_dataset('my_train.record')
-        data_set_train = data_set_train.shuffle(shuffle_pool_size).batch(args.batch_size).repeat()
-        data_set_train_iter = data_set_train.make_one_shot_iterator()
-        train_handle = sess.run(data_set_train_iter.string_handle)
-        # !!
-
+        # data_set_train = get_dataset('my_train.record')
+        # data_set_train = data_set_train.shuffle(shuffle_pool_size).batch(args.batch_size).repeat()
+        # data_set_train_iter = data_set_train.make_one_shot_iterator()
+        # train_handle = sess.run(data_set_train_iter.string_handle)
+        # # !!
+        train_iter = get_iter()
         imgs = tf.placeholder(tf.float32, [args.batch_size, 224, 224, 3])
         captions = tf.placeholder(tf.float32, [args.batch_size, None])
+        cnn_feats = tf.placeholder(tf.float32, [args.batch_size, 196, 512])
         cnn_model = CNN_Encoder(imgs)
         imgs_feats = cnn_model.conv5_3
-        decoder = Decoder(imgs_feats, captions)
+
+        decoder = Decoder(cnn_feats, captions)
         for epoch in range(args.epoch):
-            imgs_batch, captions_batch = sess.run([])
-            _, time_step = captions_batch.size()
-            embeddings = sess.run([decoder.concat_embedding],feed_dict={})
+            image, w, h, c, caption,caption_number, name = sess.run(fetches=train_iter)
+            image = pre_process(np.array(image).reshape(32, 256, 256, 3))
+            image = sess.run(image)
+
+            batch_size, time_step = caption.size() # ??
+            decoder.hx = tf.zeros([args.batch_size, 1024])
+            decoder.cx = tf.zeros([args.batch_size, 1024])
+            predicts = tf.zeros([args.batch_size, time_step, args.vocab_size])
+
+            s_img_feats = sess.run([imgs_feats], feed_dict={imgs: image})
+            embeddings = sess.run([decoder.concat_embedding],feed_dict={cnn_feats: s_img_feats,captions: caption_number})
             for i in range(time_step):
-                feas = sess.run([decoder.context], feed_dict = {imgs: imgs_batch,
-                                                    captions: captions_batch,
-                                                    hx: None})
+                feas = sess.run([decoder.context], feed_dict = {cnn_feats: s_img_feats,
+                                                    captions: caption_number,
+                                                    })
                 inputs = tf.concat([feas, embeddings[:,i,:] ], -1) # -1??
-                decoder.hx, decoder.cx = decoder.lstm_cell(inputs) # ???
+                decoder.hx, decoder.cx = tf.nn.dynamic_rnn(decoder.cell, inputs, initial_state=initial_state, scope='rnnlm') # ???
+                output = decoder.linear(decoder.hx)
+                predicts[:,i,:] = output
 
-            loss = None
-            sess.run(train_step)
-            if epoch % 100 == 0:
-                train_loss = sess.run([])
+            loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example([predicts], [], [tf.ones_like(targets, dtype=tf.float32)], args.vocab_size)
+            cost = tf.reduce_mean(loss)
+            learning_rate = args.learning_rate
+            tvars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 5)
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            train_op = optimizer.apply_gradients(zip(grads, tvars))
 
+            train_loss, _ = sess.run([cost, train_op],
+                                                feed_dict={})
+
+            if (epoch + 1) % 20 == 0:
+                print(epoch, "training loss: ", train_loss)
+
+            if (epoch + 1) % 50 == 0:   
+                decoder.saver().save(sess, 'CR_model/test.module')
+    print("train end!")
 
         
 
@@ -76,6 +106,7 @@ if __name__ == '__main__':
     #Moddel parameter
     parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate for optimizer')
     parser.add_argument('--batch_size', type=int, default=128, help='batch size for training ')
+    parser.add_argument('--vocab_size', type=int, default=24, help='size of vocab')
     # parser.add_argument('--', type=, default=, help='')
     # parser.add_argument('--', type=, default=, help='')
     # parser.add_argument('--', type=, default=, help='')
